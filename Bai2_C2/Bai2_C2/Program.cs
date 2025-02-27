c7608281fccdd3b4ed344e8715344598f2935d91
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-27 19:50:26 +0700

    bai 2 chuong 2

---
+++ Bai2_C2/Bai2_C2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_C2
+{
+    class Program
+    {
+        enum Animal_224
+        {
+            Cat,
+            Dog,
+            Mokey,
+            Bird,
+            Fish,
+            Tuna
+        }
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine(Enum.GetName(typeof(Animal_224),4));
+
+            foreach (var str in Enum.GetNames(typeof(Animal_224)))
+            {
+                Console.WriteLine(str);
+            }
+
+            Animal_224 anm_224;
+            Enum.TryParse<Animal_224>("2",out anm_224);
+            Console.WriteLine(anm_224);
+            Console.ReadKey();
+        }
+    }
+}
