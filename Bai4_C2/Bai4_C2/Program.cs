b022f7f7fd29457636e1498ffa992af870b67f05
Ky15012004 <nguyenphuocky2021qt@gmail.com>
2025-02-27 19:53:36 +0700

    bai 4 chuong 2

---
+++ Bai4_C2/Bai4_C2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4_C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Students_224 = new List<string>();
+            Console.WriteLine("Nhap n:");
+            int n_224 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n_224; i++)
+            {
+                Console.WriteLine($"Nhap ten sinh vien thu {i}");
+                string s = Console.ReadLine();
+                Students_224.Add(s);
+            }
+
+            Console.WriteLine("Danh sach ten hoc sinh: ");
+            foreach(var student_224 in Students_224)
+            {
+                Console.WriteLine(student_224);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
